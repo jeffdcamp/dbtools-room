8c908045342a5aeea2699257a9606c8818d0d2f4
jeffdcamp <jeffdcamp@gmail.com>
2020-06-10 16:44:02 -0600

    AGP 4.0.0 Updated versions

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -10,7 +10,7 @@ object Deps {
 
     // Code
     const val KOTLIN_STD_LIB = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:$KOTLIN_VERSION"
-    private const val COROUTINES_VERSION = "1.3.3"
+    private const val COROUTINES_VERSION = "1.3.7"
     const val COROUTINES = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$COROUTINES_VERSION"
     const val TIMBER = "com.jakewharton.timber:timber:4.7.1"
 
@@ -22,7 +22,7 @@ object Deps {
     const val XERIAL_SQLITE = "org.xerial:sqlite-jdbc:3.31.1"
 
     // Test
-    private const val JUNIT_VERSION = "5.6.1"
+    private const val JUNIT_VERSION = "5.6.2"
     const val TEST_JUNIT = "org.junit.jupiter:junit-jupiter:$JUNIT_VERSION"
     const val TEST_JUNIT_API = "org.junit.jupiter:junit-jupiter-api:$JUNIT_VERSION"
     const val TEST_JUNIT_ENGINE = "org.junit.jupiter:junit-jupiter-engine:$JUNIT_VERSION"
