e7fff4f664bcdb6a6918f91544231ac52bcdf8f1
jeffdcamp <jeffdcamp@gmail.com>
2022-10-05 16:40:41 -0600

    8.0.0-beta01

--- buildSrc/src/main/kotlin/Build.kt
+++ buildSrc/src/main/kotlin/Build.kt
@@ -8,7 +8,7 @@ object AndroidSdk {
 
 object Pom {
     const val GROUP_ID = "org.dbtools"
-    const val VERSION_NAME = "8.0.0-alpha02"
+    const val VERSION_NAME = "8.0.0-beta01"
     const val POM_DESCRIPTION = "DBTools for Room"
 
     const val URL = "https://github.com/jeffdcamp/dbtools-room/"
