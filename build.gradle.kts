eda85838c2ed4d0e74207474f5ee0459a40d8d25
jeffdcamp <jeffdcamp@gmail.com>
2025-07-31 13:57:48 -0600

    Updated build config

--- build.gradle.kts
+++ build.gradle.kts
@@ -52,7 +52,7 @@ allprojects {
         tasks.register<de.undercouch.gradle.tasks.download.Download>("downloadDetektConfig") {
             download {
                 onlyIf { !file("$projectDir/build/config/detektConfig.yml").exists() }
-                src("https://raw.githubusercontent.com/jeffdcamp/kmp-commons/master/detekt/detektConfig-latest.yml")
+                src("https://mobile-cdn.churchofjesuschrist.org/android/build/detekt/detektConfig-20231101.yml")
                 dest("$projectDir/build/config/detektConfig.yml")
             }
         }
