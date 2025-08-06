11ce3cbb0ec2d95520a2f7904efc3e18750dc032
jeffdcamp <jeffdcamp@gmail.com>
2025-08-06 08:21:53 -0600

    Simplified RoomDatabaseRepository and improved thread safety

--- dbtools-room/src/commonMain/kotlin/org/dbtools/room/database/RoomDatabaseRepository.kt
+++ dbtools-room/src/commonMain/kotlin/org/dbtools/room/database/RoomDatabaseRepository.kt
@@ -2,6 +2,9 @@ package org.dbtools.room.database
 
 import androidx.room.RoomDatabase
 import co.touchlab.kermit.Logger
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 
 /**
  * Database repository to hold key/value set of similar databases
@@ -9,69 +12,63 @@ import co.touchlab.kermit.Logger
  * ```
      * class BookDatabaseRepository: RoomDatabaseRepository<BookDatabase>(context) {
      *
-     *   override fun createDatabase(filename: String): BookDatabase {
-     *           return Room.databaseBuilder<BookDatabase>(filename)
-     *               .setDriver(BundledSQLiteDriver())
-     *               .build()
-     *    }
-     *
-     *   override fun deleteDatabase(filename: String) {
-     *           FileSystem.SYSTEM.deleteDatabaseFiles(filename.toPath())
-     *    }
-     * }
- * ```
- * // Initialize (one time)
- * bookDatabaseRepository.registerDatabase("book-a", "/sdcard/Downloads/booka")
- *
- * ...
- *
- * // Regular use
- * val bookDatabase = bookDatabaseRepository.getDatabase("book-a")
- * bookDatabase?.getAuthorDao()?.findNameById(123)
-
- *
- * ==== OR (with auto registration)
- *
- * ```
-     * class BookDatabaseRepository: RoomDatabaseRepository<BookDatabase>(context) {
+     *   override fun getDatabaseFilename(key: String): String? {
+     *       val databasePath: Path = getBookDatabasePath(key)
+     *       return if (fileSystem.exists(databasePath)) databasePath.toString() else null
+     *   }
      *
      *   override fun createDatabase(filename: String): BookDatabase {
-     *           return Room.databaseBuilder<BookDatabase>(filename)
-     *               .setDriver(BundledSQLiteDriver())
-     *               .build()
-     *    }
+     *       return Room.databaseBuilder<BookDatabase>(filename)
+     *           .setDriver(BundledSQLiteDriver())
+     *           .build()
+     *   }
      *
      *   override fun deleteDatabase(filename: String) {
-     *           FileSystem.SYSTEM.deleteDatabaseFiles(filename.toPath())
-     *    }
-     *
-     *   override fun autoRegisterDatabase(key: String): Boolean {
-     *      val databasePath: Path = GLFileUtil.getBookDatabaseFile(LangIso3(key))
-     *
-     *      if (FileSystem.SYSTEM.exists(databasePath)) {
-     *          registerDatabase(key, databasePath.toString())
-     *          return true
-     *      }
-     *
-     *      return false
+     *       FileSystem.SYSTEM.deleteDatabaseFiles(filename.toPath())
      *   }
      * }
  * ```
  *
- * // Regular use (getDatabase(...) will auto register "book-a"
+ * // Regular use
  * val bookDatabase = bookDatabaseRepository.getDatabase("book-a")
- * bookDatabase.getAuthorDao().findNameById(123)
+ * bookDatabase?.getAuthorDao()?.findNameById(123)
  *
  */
 abstract class RoomDatabaseRepository<out T: RoomDatabase> {
     private val databaseList = mutableMapOf<String, RoomDatabaseRepositoryItem<T>>()
 
+    private val creationLocks = mutableMapOf<String, Mutex>()
+    private val creationLocksMutex = Mutex()
+    private suspend fun getKeyMutex(key: String): Mutex {
+        return creationLocksMutex.withLock {
+            creationLocks.getOrPut(key) { Mutex() }
+        }
+    }
+
+    /**
+     * Get the database filename/path
+     *
+     * @param key of the database
+     * @return filename/path of the database or null if the database could not set up
+     */
+    abstract fun getDatabaseFilename(key: String): String?
+
+    /**
+     * Create a new database instance
+     *
+     * @param filename filename/path of the database
+     */
     protected abstract fun createDatabase(filename: String): T
+
+    /**
+     * Delete the database file
+     *
+     * @param filename filename/path of the database
+     */
     protected abstract fun deleteDatabase(filename: String)
 
     /**
      * Get database from internal repository.
-     * NOTE: override autoRegisterDatabase(...) function to allow the getDatabase(...) function to auto register a database
      *
      * @param key of the database
      * @return RoomDatabase or null if the database does not exist
@@ -82,71 +79,41 @@ abstract class RoomDatabaseRepository<out T: RoomDatabase> {
             return null
         }
 
-        // if database is not registered, then try to auto-register
-        if (!isDatabaseRegistered(key, true)) {
-            Logger.d { "database is not registered for key [$key]" }
-            return null
+        val database = databaseList[key]?.database
+        if (database != null) {
+            // database is already registered
+            return database
         }
 
-        return databaseList[key]?.database
-    }
+        // if database is not registered then try to register it
+        //
+        // Use runBlocking here so that getDatabase(...) does not need to be a suspend function
+        //    1. This section of code should be called VERY rarely (ONLY when first-time registration happens), so it is not a performance issue
+        //    2. This allows the caller to not have to use a suspend function (especially when using a Flow Dao function... which shouldn't be a suspend function)
+        return runBlocking {
+            getKeyMutex(key).withLock {
+                if (!isDatabaseRegistered(key)) {
+                    val filename = getDatabaseFilename(key)
+                    if (filename != null) {
+                        databaseList[key] = RoomDatabaseRepositoryItem(createDatabase(filename), filename)
+                    } else {
+                        return@withLock null
+                    }
+                }
 
-    /**
-     * Override this function to allow the getDatabase(...) function to auto register a database.  The override function needs to call registerDatabase(...)
-     *
-     * @param key of the database
-     * @return true if the database was registered
-     */
-    open fun autoRegisterDatabase(key: String) = false
+                databaseList[key]?.database
+            }
+        }
+    }
 
     /**
      * Identify if the database has been added to the internal repository
      * NOTE: override autoRegisterDatabase(...) function to allow the isDatabaseRegistered(...) function to auto register a database
      *
      * @param key of the database
-     * @param autoRegister if the database has not yet been added to the databaseList, then auto-add it if missing
      * @return true if the database is added to the repository
      */
-    open fun isDatabaseRegistered(key: String, autoRegister: Boolean = true): Boolean {
-        val registered = databaseList.containsKey(key)
-
-        return when {
-            registered -> true
-            !registered && autoRegister -> internalAutoRegister(key)
-            else -> false
-        }
-    }
-
-    /*
-     * This function will call the override function autoRegisterDatabase(...)
-     * @return true if the override function autoRegisterDatabase(...) successfully registered the database (this function will verify if it actually got added AFTER)
-     */
-    private fun internalAutoRegister(key: String): Boolean {
-        if (autoRegisterDatabase(key)) {
-            // make sure it actually got added
-            return databaseList.containsKey(key)
-        }
-
-        return false
-    }
-
-    /**
-     * Add database to internal repository
-     *
-     * @param key of the database
-     * @param filename of the database
-     * @return true if the database was added
-     */
-    open fun registerDatabase(key: String, filename: String): Boolean {
-        if (isDatabaseRegistered(key, false)) {
-            Logger.w { "Database already registered for key [$key]" }
-            return false
-        }
-
-        databaseList[key] = RoomDatabaseRepositoryItem(createDatabase(filename), filename)
-
-        return true
-    }
+    fun isDatabaseRegistered(key: String): Boolean = databaseList.containsKey(key)
 
     /**
      * Close the database and remove the reference from the WrapperRepository
@@ -180,9 +147,7 @@ abstract class RoomDatabaseRepository<out T: RoomDatabase> {
      * @param deleteFiles Delete the database file and any associated files to this database (default: false)
      */
     open fun closeAllDatabases(deleteFiles: Boolean = false) {
-        val keysCopy = ArrayList<String>()
-        keysCopy.addAll(databaseList.keys)
-        keysCopy.forEach { key -> closeDatabase(key, deleteFiles) }
+        databaseList.keys.forEach { key -> closeDatabase(key, deleteFiles) }
     }
 }
 
